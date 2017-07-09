a3e177f6343d5ee6690a15a418a40eea504e3263
Ivan Bovkush <ivan.bovkush@gmail.com>
2017-07-10 00:31:36 +0300

    Small fix for filters was implemented.

--- testAppNode/js/filters/filters.js
+++ testAppNode/js/filters/filters.js
@@ -3,6 +3,7 @@
         return function (value) {
             var md = moment.duration(value);
             var ms = (md.milliseconds() / 10).toFixed(0);
+            ms = ms == 100 ? 99 : ms;
             ms = ms < 10 ? "0" + ms : ms;
             return md.format("mm:ss.", { trim: false}) + ms;
         };
@@ -12,7 +13,9 @@
         return function (md) {
             var ms = (md.milliseconds() / 10).toFixed(0);
             var format = ms > 50 ? "mm:ss." : "mm ss ";
+            ms = ms == 100 ? 99 : ms;
             ms = ms < 10 ? "0" + ms : ms;
             return md.format(format, { trim: false}) + ms;
         };
     });
+
